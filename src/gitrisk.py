5c9fef117199d42122c1646a58b2960a6588f509
Scott Johnson <jaywir3@gmail.com>
2014-06-29 11:35:23 -0500

    Issue #1: Add test infrastructure in a more robust way.

--- src/gitrisk.py
+++ src/gitrisk.py
@@ -3,19 +3,25 @@ import configparser
 import sys
 import re
 
+class GitRisk:
+  mSpecString = None
+
+  def __init__(self, aSpecString):
+    self.mSpecString = aSpecString
+
+  def _runTestWithFile(self, aFileName, aRegex):
+    f = open(aFileName)
+    for line in f:
+      result = re.search(aRegex, line)
+      print(result.group(0))
+
 def createParser():
   parser = argparse.ArgumentParser(description='''
-    Parse git log files for potential regression risks after a merge
+  Parse git log files for potential regression risks after a merge
   ''', add_help=True)
   parser.add_argument('-c', '--config', dest='confFile', help='Specify a configuration file', action='store')
   parser.add_argument('-t', '--test', dest='testFile', help='Specify a test file to test against with the given config file', action='store', default=False)
   return parser
-
-def runTestWithFile(aFileName, aRegex):
-  f = open(aFileName)
-  for line in f:
-    result = re.search(aRegex, line)
-    print(result.group(0))
 
 def main():
   parser = createParser()
@@ -27,8 +33,9 @@ def main():
   config = configparser.SafeConfigParser()
   config.read(parsedArgs.confFile)
   searchString = config.get('main', 'ticket-spec')
+  gitrisk = GitRisk(searchString)
   if parsedArgs.testFile:
-      runTestWithFile(parsedArgs.testFile, searchString)
+      gitrisk._runTestWithFile(parsedArgs.testFile, searchString)
 
 if __name__ == '__main__':
   main()
---
+++ src/test/testSuite.py
@@ -0,0 +1,4 @@
+import imp
+
+gitRisk = imp.load_source('gitrisk', '../gitrisk.py')
+print(dir(gitRisk))
