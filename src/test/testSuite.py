d6044e9292299bb761f751d881f532e3359dd40c
Scott Johnson <jaywir3@gmail.com>
2014-06-29 12:17:05 -0500

    Issue #2: Make testSuite unzip src/test/data/testrepo.zip into a temporary directory before beginning tests.

--- src/test/testSuite.py
+++ src/test/testSuite.py
@@ -1,9 +1,30 @@
 import imp
 import unittest
+import zipfile
+import os.path
+import os
+import tempfile
+import shutil
 
 class TestSpecifications(unittest.TestCase):
   def setUp(self):
+    self.mTempDir = tempfile.mkdtemp(prefix='gitRiskTest')
+    self.assertTrue(os.path.exists(self.mTempDir))
+    zipFh = open('data/testRepo.zip', 'rb')
+    testRepoZip = zipfile.ZipFile(zipFh)
+    for name in testRepoZip.namelist():
+      testRepoZip.extract(name, self.mTempDir)
+    zipFh.close()
+
+    # The temp directory should be populated and contain
+    # at least a git-risk-info file.
+    pathToTestRepo = os.path.join(self.mTempDir, 'testRepo')
+    self.assertTrue(os.path.exists(os.path.join(pathToTestRepo, "git-risk-info")))
     self.mGitRiskModule = imp.load_source('gitrisk', '../gitrisk.py')
+
+  def tearDown(self):
+    shutil.rmtree(self.mTempDir)
+    self.assertFalse(os.path.exists(self.mTempDir))
 
   def test_JMTicketNames(self):
     gitRisk = self.mGitRiskModule.GitRisk("(JM|jm)-[0-9]+")
